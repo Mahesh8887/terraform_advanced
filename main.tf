84e4087dc0bcf840c38d6358002a8f39a5a03d82
Mahesh8887 <119730245+Mahesh8887@users.noreply.github.com>
2023-05-01 23:27:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,9 @@
-terraform {
-        required_providers {
-
-        aws =  {
-                source = "hashicorp/aws"
-                version = ">= 4.16"
-                }
-        }
-        required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-        region = "us-east-1"
-}
-
 resource "aws_instance" "myfirstinstance" {
         count = 2
-        ami = "ami-007855ac798b5175e"
-        instance_type = "t2.micro"
+        ami = var.ami_id
+        instance_type = var.instance_type
         tags = {
-                Name = "Terraform_Server1"
+                Name = var.tag_name
                 }
 }
-
-output "ec2_public_ips" {
-        value = aws_instance.myfirstinstance[*].public_ip
-        }
                                            
