b6092ae4726f0cd102cff5ebf72eaa473e268488
Mahesh8887 <119730245+Mahesh8887@users.noreply.github.com>
2023-05-01 23:49:23 +0530

    Update jenkin.groovy

--- jenkin.groovy
+++ jenkin.groovy
@@ -14,6 +14,11 @@ pipeline {
                 sh 'terraform init' 
             }
         }
+        stage('terraform validate'){
+            steps{
+                sh 'terraform validate' 
+            }
+        }
            stage('terraform plan'){
             steps{
                 sh 'terraform plan' 
