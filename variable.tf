8a93c3af4c5b9aea9368a0273a906fc99eff00db
Mahesh8887 <119730245+Mahesh8887@users.noreply.github.com>
2023-05-01 18:35:19 +0530

    fiest

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "access_key" {
+  default     = ""
+}
+variable "secret_key" {
+  default     = ""
+}
+variable "region" {
+  default     = "us-east-1"
+}
+variable "tag_name" {
+  description = "Tag Name of for Ec2 Instance"  
+  default     = "my_first_ec2_instance"
+}
+variable "ami_id" {
+  description = "AMI for Ubuntu EC2 Instance"  
+  default     = ""
+}
+variable "instance_type" {
+  description = "instance type for Ec2"
+  default     = "t2.micro"
+}
