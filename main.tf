405a3e933a98e0b4779d7d5548ea01c3dfbe2264
Mahesh8887 <119730245+Mahesh8887@users.noreply.github.com>
2023-05-01 14:56:22 +0530

    main

--- main.tf
+++ main.tf
@@ -1,4 +1,36 @@
-{
+terraform {
+        required_providers {
 
-    kkbj
+        aws =  {
+                source = "hashicorp/aws"
+                version = ">= 4.16"
+                }
+        }
+        required_version = ">= 1.2.0"
 }
+
+provider "aws" {
+        region = "us-east-1"
+}
+
+resource "aws_instance" "myfirstinstance" {
+        count = 2
+        ami = "ami-007855ac798b5175e"
+        instance_type = "t2.micro"
+        tags = {
+                Name = "Terraform_Server1"
+                }
+}
+
+resource "aws_s3_bucket" "bucket_form_terraform_by_MVB" {
+        bucket = "aws-s3-bucket.bucket-form-terraform.mahesh"
+        tags = {
+                Name = "bucket_from_terraform_by_mahesh"
+                Environment = "DevQA"
+                }
+}
+
+output "ec2_public_ips" {
+        value = aws_instance.myfirstinstance[*].public_ip
+        }
+                                           
