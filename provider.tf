e2060f389d79099c8fb3b64940f1187ebce846b4
Mahesh8887 <119730245+Mahesh8887@users.noreply.github.com>
2023-05-01 22:56:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
