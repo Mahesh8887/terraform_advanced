d75cda389f9cd2866bb3c6c5679a961bac7d4061
Mahesh8887 <119730245+Mahesh8887@users.noreply.github.com>
2023-05-02 15:40:49 +0530

    Update jenkin.groovy

--- jenkin.groovy
+++ jenkin.groovy
@@ -6,7 +6,7 @@ pipeline {
     stages{
         stage('Git Code checkout'){
             steps{
-                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github', url: 'https://github.com/Mahesh8887/terraform_advanced.git']]) 
+                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'jenkin', url: 'https://github.com/Mahesh8887/terraform_advanced.git']])
             }
         }
         stage('terraform init'){
