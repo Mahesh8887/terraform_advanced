c8e92c50a60f17bc5b58cc86b166b29965bb0fd7
Mahesh8887 <119730245+Mahesh8887@users.noreply.github.com>
2023-05-02 16:33:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "myfirstinstance" {
-        count = 2
-        ami = var.ami_id
-        instance_type = var.instance_type
-        tags = {
-                Name = var.tag_name
-                }
+   count = 2
+   ami = var.ami_id
+   instance_type = var.instance_type
+   tags = {
+     Name = var.tag_name
+   }
 }
                                            
