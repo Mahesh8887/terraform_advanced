854debcffdd894d671be5b4ca4621572be25f61a
Mahesh8887 <119730245+Mahesh8887@users.noreply.github.com>
2023-05-02 17:09:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 resource "aws_instance" "myfirstinstance" {
+  count = 2
   ami = var.ami_id
   instance_type = var.instance_type
   tags = {
     Name = var.tag_name
   }
 }                                         
+
+output "ec2_public_ips" {
+        value = aws_instance.myfirstinstance[*].public_ip
+        }
+                                           
